6080c94c4b05b82c3fe21fbff9300833921bea0c
Kamil Dydo <dydokamil@gmail.com>
2017-08-21 13:21:40 +0200

    Added grid search

--- lake2.py
+++ lake2.py
@@ -3,13 +3,8 @@ import numpy as np
 
 env = gym.make("FrozenLake-v0")
 
-EPISODES = 5000
-ALPHA = .1
-GAMMA = .9  # discounted feature reward
 
-Q = np.zeros((env.observation_space.n, env.action_space.n))
-
-def get_best_action(state):
+def get_best_action(Q, state):
     return np.argmax(Q[state, :])
 
 
@@ -17,34 +12,51 @@ def get_random_action():
     return env.action_space.sample()
 
 
-def get_max_policy(state):
+def get_max_policy(Q, state):
     return np.max(Q[state, :])
 
 
-for i in range(EPISODES):
-    s = env.reset()
-    terminated = False
-    while not terminated:
-        a = get_random_action()
-        s_prime, r, terminated, _ = env.step(a)
-        if not terminated:
-            s = s_prime
+def run(alpha, gamma, episodes):
+    # training
+    Q = np.zeros((env.observation_space.n, env.action_space.n))
+    for i in range(episodes):
+        s = env.reset()
+        terminated = False
+        while not terminated:
+            a = get_random_action()
+            s_prime, r, terminated, _ = env.step(a)
+            if not terminated:
+                s = s_prime
 
-    R = 100 if r == 1 else -100
-    Q[s, a] += ALPHA * (R + GAMMA * get_max_policy(s_prime) - Q[s, a])
+        R = 100 if r == 1 else -100
+        Q[s, a] += alpha * (R + gamma * get_max_policy(Q, s_prime) - Q[s, a])
 
-print(Q)
+    # evaluating
+    won = 0
+    for i in range(episodes):
+        s = env.reset()
+        terminated = False
+        while not terminated:
+            a = get_best_action(Q, s)
+            s_prime, r, terminated, _ = env.step(a)
+            if not terminated:
+                s = s_prime
+        if r == 1:
+            won += 1
 
-won = 0
-for i in range(EPISODES):
-    s = env.reset()
-    terminated = False
-    while not terminated:
-        a = get_best_action(s)
-        s_prime, r, terminated, _ = env.step(a)
-        if not terminated:
-            s = s_prime
-    if r == 1:
-        won += 1
+    return won
 
-print("Won", won, 'times')
+
+def grid_search(alpha, gamma, episodes):
+    results = []
+    for a in alpha:
+        for g in gamma:
+            results.append([a, g, run(a, g, episodes)])
+    return results
+
+
+results = grid_search(np.linspace(0, 1, 11), np.linspace(0, 1, 11), 5000)
+results = np.asarray(results)
+best = results[np.argmax(results[:, 2])]
+print(f'Best alpha: {best[0]}, best gamma: {best[1]}, won: {int(best[2])} times.')
+
